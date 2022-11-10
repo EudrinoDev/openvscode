267f8c249a5ef6f6dbff187848439979c4e0d578
EudrinoDev <57170855+EudrinoDev@users.noreply.github.com>
2022-11-10 22:27:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec $OPENVSCODE_SERVER_ROOT/bin/openvscode-server --folder /home/cargo-template --port $PORT --host 0.0.0.0 -- $*
+
+exec $OPENVSCODE_SERVER_ROOT/bin/openvscode-server --default-folder /home/cargo-template --port $PORT --connection-token $TOKEN= --host 0.0.0.0 -- $*
